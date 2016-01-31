62b872b47a81893c0eacaadc9e82577072946bc6
samanth91 <samnagul@umail.iu.edu>
2016-01-31 20:51:55 +0000

    cookbooks and database.rb

--- roletest/base.rb
+++ roletest/base.rb
@@ -1,3 +1,3 @@
 name "base"
 description "role to run all nodes"
-run_list "recipe[security]", "recipe[localusers]"
+run_list "recipe[chef-client::delete_validation]","recipe[chef-client::cron]","recipe[chef-client]","recipe[localusers]"
